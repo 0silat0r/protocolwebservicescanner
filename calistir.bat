d7cc78e4a55e9a9f75130cb0e0b5244c5564746c
Shad0wE4 <87228325+Sh4d0wE4@users.noreply.github.com>
2022-01-06 22:34:16 +0300

    Add files via upload

---
+++ calistir.bat
@@ -0,0 +1,10 @@
+@echo off
+title ProtocolWebServiceScannerV1
+color a 
+echo Hos geldiniz..
+timeout 5
+echo Lutfen bekleyiniz..
+timeout 5
+cls
+C:/Users/pcmfk/AppData/Local/Microsoft/WindowsApps/python3.9.exe d:/simplepythonprogram1/project1.py
+pause
