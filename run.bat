b2c8736f811196e2ca4fa7bc37d89a02ef6a3c24
Shad0wE4 <87228325+Sh4d0wE4@users.noreply.github.com>
2022-01-06 23:08:08 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
 @echo off
 title ProtocolWebServiceScannerV1
 color a 
-echo Hos geldiniz..
+echo Welcome..
 timeout 5
-echo Lutfen bekleyiniz..
+echo Please Hold On..
 timeout 5
 cls
+echo Programming By Sh4d0wE4
+timeout 4
 C:/Users/pcmfk/AppData/Local/Microsoft/WindowsApps/python3.9.exe d:/simplepythonprogram1/project1.py
 pause
